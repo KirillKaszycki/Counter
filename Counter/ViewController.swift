45c3c7372adaf428e8986ecdaaa04e4645086d1e
KirillKaszycki <kashitskii.kirill@gmail.com>
2024-01-09 18:06:39 +0500

    Добавлена логика кнопок "+", "-", "Сброс". история действий с датой и временем, заблокирована возможность уменьшать счётчик ниже нуля и редактировать историю действий

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,69 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelText: UILabel!
+    @IBOutlet weak var buttonPlusOne: UIButton!
+    @IBOutlet weak var buttonMinusOne: UIButton!
+    @IBOutlet weak var buttonReset: UIButton!
+    
+    @IBOutlet weak var historyTextViewer: UITextView!
+    
+    private var counter: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        labelText.text = "Значение счётчика: \(counter)"
+        buttonPlusOne.setTitle("+", for: .normal)
+        buttonMinusOne.setTitle("-", for: .normal)
+        buttonReset.setTitle("Сброс", for: .normal)
+        
+        buttonPlusOne.tintColor = .red
+        buttonReset.tintColor = .green
+        
+        historyTextViewer.text = "История изменений:"
+        historyTextViewer.isEditable = false // Блокировка возможности редактировать 
     }
 
-
+    // Логика кнопки +
+    @IBAction func plusOnePushed(_ sender: Any) {
+        counter += 1
+        labelText.text = "Значение счётчика: \(counter)"
+        
+        logHistory("Значение изменено на +1")
+    }
+    
+    // Логика кнопки -
+    @IBAction func minusOnePushed(_ sender: Any) {
+        if counter > 0 {
+            counter -= 1
+            labelText.text = "Значение счётчика: \(counter)"
+            logHistory("Значение изменено на -1")
+        } else {
+            logHistory("попытка уменьшить значение счётчика ниже 0")
+        }
+    }
+    
+    // Логика кнопки сброса
+    @IBAction func resetLabelCounter(_ sender: Any) {
+        counter = 0
+        labelText.text = "Значение счётчика: \(counter)"
+        
+        logHistory("Значение сброшено")
+    }
+    
+    // Логика поля истории нажатий
+    private func logHistory(_ message: String) {
+        let dateFormater = DateFormatter()
+        dateFormater.dateFormat = "dd-MM-yyyy HH:mm"
+        let dateString = dateFormater.string(from: Date())
+        
+        let historyEntry = "\n\(dateString): \(message)"
+        historyTextViewer.text.append(historyEntry)
+        
+        let range = NSMakeRange(historyTextViewer.text.count - 1, 1)
+        historyTextViewer.scrollRangeToVisible(range)
+    }
+    
 }
 
